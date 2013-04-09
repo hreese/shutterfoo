1e8944c34d63f2c6ce8aee040074d756e78b13c8
Heiko Reese <mail@heiko-reese.de>
2013-04-10 00:36:09 +0200

    Moved file as per arduino convention. Added comments.

--- valve/valve.ino
+++ valve/valve.ino
@@ -1,5 +1,10 @@
+// I/O-Pin for shutter release
 int shutter = 13;
+// I/O-Pin for valve release
 int valve = 12;
+// time between opening and closing the selenoid valve 
+// to create a single drop
+int drop_time_len = 25;
 
 void setup() {                
   pinMode(shutter, OUTPUT);     
@@ -9,16 +14,23 @@ void setup() {
 void loop() {
   delay(4000);
   
+  // First drop
   digitalWrite(valve, HIGH);
-  delay(25);
+  delay(drop_time_len);
   digitalWrite(valve, LOW);
+
+  // wait this long before next drop
   delay(100);
-  
+
+  // Second drop
   digitalWrite(valve, HIGH);
-  delay(25);
+  delay(drop_time_len);
   digitalWrite(valve, LOW);
+
+  // wait this long for collision
   delay(65);
-  
+
+  // take picture
   digitalWrite(shutter, HIGH);
   delay(100);
   digitalWrite(shutter, LOW);
